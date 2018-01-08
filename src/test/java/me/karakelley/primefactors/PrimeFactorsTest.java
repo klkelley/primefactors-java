de53572c05d238bc732342c34eba0929692e4192
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-08 08:59:17 -0600

    Add failing tests for calculating factors of three

--- src/test/java/me/karakelley/primefactors/PrimeFactorsTest.java
+++ src/test/java/me/karakelley/primefactors/PrimeFactorsTest.java
@@ -15,6 +15,16 @@ class PrimeFactorsTest {
     assertEquals(list(), PrimeFactors.calculate(1));
   }
 
+  @Test
+  public void testFactorsOfTwo() {
+    assertEquals(list(2), PrimeFactors.calculate((2)));
+  }
+
+  @Test
+  public void testFactorsOfThree() {
+    assertEquals(list(3), PrimeFactors.calculate(3));
+  }
+
   private List<Integer> list(int...ints) {
     ArrayList<Integer> list = new ArrayList<Integer>();
     for ( int i : ints) {
