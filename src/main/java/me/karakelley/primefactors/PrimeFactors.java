c03d3c0d0b0d5371dc6813c94296cbb767eab88e
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-09 08:18:34 -0600

    Pass second test for calculating prime factors of 2

--- src/main/java/me/karakelley/primefactors/PrimeFactors.java
+++ src/main/java/me/karakelley/primefactors/PrimeFactors.java
@@ -7,6 +7,14 @@ public class PrimeFactors {
 
   public static List<Integer> calculate(int number) {
     ArrayList<Integer> primeFactors = new ArrayList<Integer>();
+    int possibleFactors = 2;
+    while (number > 1) {
+      while (number % possibleFactors == 0) {
+        primeFactors.add(possibleFactors);
+        number /= possibleFactors;
+      }
+      possibleFactors++;
+    }
     return primeFactors;
   }
 }
