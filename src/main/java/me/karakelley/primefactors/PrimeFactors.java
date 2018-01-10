e8bbf0a8e83ea089e2e6876b623d8680efc8d91e
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-10 08:19:18 -0600

    Complete kata with passing tests

--- src/main/java/me/karakelley/primefactors/PrimeFactors.java
+++ src/main/java/me/karakelley/primefactors/PrimeFactors.java
@@ -1,5 +1,20 @@
 package me.karakelley.primefactors;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeFactors {
 
+  public static List<Integer> calculate(int number) {
+    ArrayList<Integer> primeFactors = new ArrayList<Integer>();
+    int possibleFactor = 2;
+    while (number > 1) {
+      while (number % possibleFactor == 0) {
+        primeFactors.add(possibleFactor);
+        number /= possibleFactor;
+      }
+      possibleFactor++;
+    }
+    return primeFactors;
+  }
 }
--- src/test/java/me/karakelley/primefactors/PrimeFactorsTest.java
+++ src/test/java/me/karakelley/primefactors/PrimeFactorsTest.java
@@ -1,7 +1,34 @@
-package me.karakelley.primefactors
+package me.karakelley.primefactors;
 
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PrimeFactorsTest {
-  
+
+  static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][] {
+            {1, Collections.emptyList()},
+            {2, Collections.singletonList(2)},
+            {4, Arrays.asList(2, 2)},
+            {6, Arrays.asList(2, 3)},
+            {9, Arrays.asList(3, 3)},
+            {27, Arrays.asList(3, 3, 3)},
+            {625, Arrays.asList(5, 5, 5, 5)}
+    });
+  }
+
+  @ParameterizedTest
+  @MethodSource("data")
+  void testNumber(int number, List<Integer> expectedOutput) {
+    assertEquals(expectedOutput, PrimeFactors.calculate(number));
+  }
 
 }
